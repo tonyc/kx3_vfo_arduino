34e29401a27220667feec74582076312b3cf8e5d
Tony Collen <tony@halogenlabs.com>
2015-01-29 20:21:21 -0600

    update sketch to have a startup sequence to make it easier to program

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,15 +1,38 @@
-
 #define LED_PIN 13
 
 void setup()
 {
-    pinMode(LED_PIN, OUTPUT);
+  delay(2000);
+
+  int i = 0;
+
+  for (i; i < 3; i++) {
+    blink();
+  }
+
+  ledOn();
+  delay(2000);
 }
 
 void loop()
 {
-    digitalWrite(LED_PIN, HIGH);
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(900);
+  ledOff();
+  delay(125);
+  ledOn();
+  delay(125);
+}
+
+void ledOn() {
+  digitalWrite(LED_PIN, HIGH);
+}
+
+void ledOff() {
+  digitalWrite(LED_PIN, LOW);
+}
+
+void blink() {
+  ledOn();
+  delay(250);
+  ledOff();
+  delay(250);
 }
